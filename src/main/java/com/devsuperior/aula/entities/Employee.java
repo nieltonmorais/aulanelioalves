47342912c617176a68a0eecc48cd75e46c21a10b
Nielton Morais <nielton.morais@gmail.com>
2024-05-05 19:34:16 -0300

    first commit

---
+++ src/main/java/com/devsuperior/aula/entities/Employee.java
@@ -0,0 +1,34 @@
+package com.devsuperior.aula.entities;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Employee {
+
+    private String name;
+    private Double grossSalary;
+
+    public Employee(){
+    }
+
+    public Employee(String name, Double grossSalary) {
+        this.name = name;
+        this.grossSalary = grossSalary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getGrossSalary() {
+        return grossSalary;
+    }
+
+    public void setGrossSalary(Double grossSalary) {
+        this.grossSalary = grossSalary;
+    }
+}
---
+++ src/main/java/com/devsuperior/aula/services/SalaryService.java
@@ -0,0 +1,24 @@
+package com.devsuperior.aula.services;
+
+import com.devsuperior.aula.entities.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SalaryService {
+
+    @Autowired
+    private TaxService taxService;
+
+    @Autowired
+    private PensionService pensionService;
+
+    @Autowired
+    private Employee employee;
+
+    public double netSalary(Employee employee){
+        return employee.getGrossSalary() -
+                taxService.tax(employee.getGrossSalary()) -
+                pensionService.discount(employee.getGrossSalary());
+    }
+}
